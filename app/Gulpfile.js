e6a4b88f7942467c74001da9a91b97819d079aa6
Javier Cervantes <javier@sawyereffect.com>
2016-04-07 15:14:29 -0500

    Exit after done in test gulp taks

--- app/Gulpfile.js
+++ app/Gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
+var exit = function() { process.exit(0); };
 
-gulp.task('test', function (done) {
+gulp.task('test', function () {
   new Server({
     configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
+  }, exit).start();
 });
